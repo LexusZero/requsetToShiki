e1d3eddf51db56c9ed62c301f83bbe213b459771
Lexus <alekcey.suhovey@gmail.com>
2022-08-05 17:09:39 +0300

    small optimization CSV request animeByName

--- requsetToShiki/RequestCsv.cs
+++ requsetToShiki/RequestCsv.cs
@@ -10,17 +10,19 @@ namespace RequestToShiki
         // /xill47/a1255ce9b6f7a3482405e7141fb5cf25/raw/95a2fbcc54385d6636e915afd2bee96e9422ca52/animes.csv
         public async Task<Anime> AnimesByName(string name)
         {
-            var animeRecords = await GetRecord(Path);
-            var foundAnime = animeRecords.FirstOrDefault(anim => anim.Name == name);
-            if (foundAnime == null)
+            var storageDataAnime = await GetRecordByName(Path, name);
+
+            if (storageDataAnime == null)
             {
                 return null;
             }
+
             var anime = new Anime()
             {
-                Name = foundAnime.Name,
-                Description = foundAnime.Description
+                Name = storageDataAnime.Name,
+                Description = storageDataAnime.Description
             };
+
             return anime;
 
         }
@@ -28,13 +30,17 @@ namespace RequestToShiki
         {
             throw new NotImplementedException();
         }
-        public async Task<IEnumerable<StorageData>> GetRecord(string requestPath)
+        public async Task<StorageData> GetRecordByName(string requestPath, string name)
         {
 
             var record = await this.client.GetStreamAsync(requestPath);
             using var streamReader = new StreamReader(record);
             using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
-            return csvReader.GetRecords<StorageData>().ToList();
+            var animeRecords = csvReader.GetRecords<StorageData>();
+            var foundAnime = animeRecords.FirstOrDefault(anim => anim.Name.Contains(
+                name, StringComparison.InvariantCultureIgnoreCase));
+            return foundAnime;
+
 
         }
     }
